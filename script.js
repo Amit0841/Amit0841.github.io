02ce5d18762b2d3868f12b0ba3f18d1127f021ff
Amit0841 <aroy16063@gmail.com>
2023-10-10 12:07:13 +0530

    Done

--- script.js
+++ script.js
@@ -49,27 +49,27 @@ $(document).ready(function () {
     });
   
     // owl carousel script
-    $(".carousel").owlCarousel({
-      margin: 20,
-    loop: false,
-      autoplay: false,   //for carousel effect turn it true
-      autoplayTimeOut: 2000,
-      autoplayHoverPause: true,
-      responsive: {
-        0: {
-          items: 1,
-          nav: false,
-        },
-        600: {
-          items: 2,
-          nav: false,
-        },
-        1000: {
-          items: 3,
-          nav: false,
-        },
-      },
-    });
+    // $(".carousel").owlCarousel({
+    //   margin: 20,
+    // loop: false,
+    //   autoplay: false,   //for carousel effect turn it true
+    //   autoplayTimeOut: 2000,
+    //   autoplayHoverPause: true,
+    //   responsive: {
+    //     0: {
+    //       items: 1,
+    //       nav: false,
+    //     },
+    //     600: {
+    //       items: 2,
+    //       nav: false,
+    //     },
+    //     1000: {
+    //       items: 3,
+    //       nav: false,
+    //     },
+    //   },
+    // });
   });
   document.getElementById("resume-button-1").addEventListener("click", function() {
     var pdfUrl = "https://drive.google.com/file/d/1OMZLxgfJOqTU1Nc9kseNOKt1dqNRFuMF/view?usp=sharing";
